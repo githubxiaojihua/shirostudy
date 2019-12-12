14c2cfdc19952b556c3395a32f18ddda91cf62ea
xiaojihua <2542213767@qq.com>
2019-12-12 18:33:21 +0800

    20191212

---
+++ shirostudy05/src/main/java/com/xiaojihua/service/PasswordHelper.java
@@ -0,0 +1,35 @@
+package com.xiaojihua.service;
+
+import com.xiaojihua.entity.User;
+import org.apache.shiro.crypto.RandomNumberGenerator;
+import org.apache.shiro.crypto.SecureRandomNumberGenerator;
+import org.apache.shiro.crypto.hash.SimpleHash;
+import org.apache.shiro.util.ByteSource;
+
+/**
+ * 密码加密帮助类
+ */
+public class PasswordHelper {
+
+    private RandomNumberGenerator randomNumberGenerator = new SecureRandomNumberGenerator();
+
+    private String algorithmName = "md5";
+    private final int hashIterations = 2;
+
+    /**
+     * 根据传入的User对象加密并且设置哈希后的密码
+     * @param user
+     */
+    public void encryptPassword(User user) {
+
+        user.setSalt(randomNumberGenerator.nextBytes().toHex());
+
+        String newPassword = new SimpleHash(
+                algorithmName,
+                user.getPassword(),
+                ByteSource.Util.bytes(user.getCredentialsSalt()),
+                hashIterations).toHex();
+
+        user.setPassword(newPassword);
+    }
+}
