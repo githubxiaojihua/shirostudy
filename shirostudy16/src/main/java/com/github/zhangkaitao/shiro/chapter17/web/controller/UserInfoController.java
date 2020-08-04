a87033b48c29a2a1d66ffdac7c6e6c392cd35ddb
xiaojihua <240971505@qq.com>
2020-08-04 15:01:00 +0800

    20200804

---
+++ shirostudy16/src/main/java/com/github/zhangkaitao/shiro/chapter17/web/controller/UserInfoController.java
@@ -0,0 +1,89 @@
+package com.github.zhangkaitao.shiro.chapter17.web.controller;
+
+import com.github.zhangkaitao.shiro.chapter17.Constants;
+import com.github.zhangkaitao.shiro.chapter17.service.OAuthService;
+import org.apache.oltu.oauth2.common.OAuth;
+import org.apache.oltu.oauth2.common.error.OAuthError;
+import org.apache.oltu.oauth2.common.exception.OAuthProblemException;
+import org.apache.oltu.oauth2.common.exception.OAuthSystemException;
+import org.apache.oltu.oauth2.common.message.OAuthResponse;
+import org.apache.oltu.oauth2.common.message.types.ParameterStyle;
+import org.apache.oltu.oauth2.common.utils.OAuthUtils;
+import org.apache.oltu.oauth2.rs.request.OAuthAccessResourceRequest;
+import org.apache.oltu.oauth2.rs.response.OAuthRSResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 资源控制器
+ * <p>User: Zhang Kaitao
+ * <p>Date: 14-2-16
+ * <p>Version: 1.0
+ */
+@RestController
+public class UserInfoController {
+
+    @Autowired
+    private OAuthService oAuthService;
+
+    @RequestMapping("/userInfo")
+    public HttpEntity userInfo(HttpServletRequest request) throws OAuthSystemException {
+        try {
+
+            //1、构建OAuth资源请求，从请求参数串中查询相关数据，也可以从请求头和body中查询
+            OAuthAccessResourceRequest oauthRequest = new OAuthAccessResourceRequest(request, ParameterStyle.QUERY);
+            //2、获取Access Token
+            String accessToken = oauthRequest.getAccessToken();
+
+            //3、验证Access Token
+            if (!oAuthService.checkAccessToken(accessToken)) {
+                // 如果不存在/过期了，返回未验证错误，需重新验证
+                OAuthResponse oauthResponse = OAuthRSResponse
+                        .errorResponse(HttpServletResponse.SC_UNAUTHORIZED)
+                        .setRealm(Constants.RESOURCE_SERVER_NAME)
+                        .setError(OAuthError.ResourceResponse.INVALID_TOKEN)
+                        .buildHeaderMessage();
+
+                HttpHeaders headers = new HttpHeaders();
+                headers.add(OAuth.HeaderType.WWW_AUTHENTICATE, oauthResponse.getHeader(OAuth.HeaderType.WWW_AUTHENTICATE));
+                return new ResponseEntity(headers, HttpStatus.UNAUTHORIZED);
+            }
+            //4、验证通过返回用户名
+            String username = oAuthService.getUsernameByAccessToken(accessToken);
+            return new ResponseEntity(username, HttpStatus.OK);
+        } catch (OAuthProblemException e) {
+            //检查是否设置了错误码
+            String errorCode = e.getError();
+            if (OAuthUtils.isEmpty(errorCode)) {
+                OAuthResponse oauthResponse = OAuthRSResponse
+                        .errorResponse(HttpServletResponse.SC_UNAUTHORIZED)
+                        .setRealm(Constants.RESOURCE_SERVER_NAME)
+                        .buildHeaderMessage();
+
+                HttpHeaders headers = new HttpHeaders();
+                headers.add(OAuth.HeaderType.WWW_AUTHENTICATE, oauthResponse.getHeader(OAuth.HeaderType.WWW_AUTHENTICATE));
+                return new ResponseEntity(headers, HttpStatus.UNAUTHORIZED);
+            }
+
+            OAuthResponse oauthResponse = OAuthRSResponse
+                    .errorResponse(HttpServletResponse.SC_UNAUTHORIZED)
+                    .setRealm(Constants.RESOURCE_SERVER_NAME)
+                    .setError(e.getError())
+                    .setErrorDescription(e.getDescription())
+                    .setErrorUri(e.getUri())
+                    .buildHeaderMessage();
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(OAuth.HeaderType.WWW_AUTHENTICATE, oauthResponse.getHeader(OAuth.HeaderType.WWW_AUTHENTICATE));
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+    }
+}
